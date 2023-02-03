e407394c384c0aee948415b5103b5591543a5533
ROCKTAKEY <rocktakey@gmail.com>
2023-02-03 18:31:39 +0900

    Initial commit

---
+++ guixi.el
@@ -0,0 +1,38 @@
+;;; guixi.el --- Emacs integration for Guix  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  ROCKTAKEY
+
+;; Author: ROCKTAKEY <rocktakey@gmail.com>
+;; Keywords: tools
+
+;; Version: 0.0.0
+;; Package-Requires: ((emacs "24.1"))
+;; URL: https://github.com/ROCKTAKEY/guixi
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Emacs integration for Guix
+
+;;; Code:
+
+(defgroup guixi ()
+  "Emacs integration for Guix."
+  :group 'tools
+  :prefix "guixi-"
+  :link '(url-link "https://github.com/ROCKTAKEY/guixi"))
+
+(provide 'guixi)
+;;; guixi.el ends here
